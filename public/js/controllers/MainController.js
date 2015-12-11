5d9e4ea82ddcfe0875ac7510ac791934c9581fef
TheDooner64 <Robert.Muldoon.1@gmail.com>
2015-12-10 22:02:46 -0500

    Complete workshop

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -11,17 +11,23 @@ app.controller('MainController', function (FlashCardsFactory, $scope) {
 
     $scope.flashCards = [];
 
+    $scope.loading;
+
     $scope.getCategoryCards = function (theCategory) {
         $scope.currentCategory = theCategory;
+        $scope.loading = true;
         FlashCardsFactory.getFlashCardsByCategory(theCategory).then(function (cards) {
             $scope.flashCards = cards;
+            $scope.loading = false;
         });
     };
 
     $scope.getAllCards = function () {
         $scope.currentCategory = null;
+        $scope.loading = true;
         FlashCardsFactory.getFlashCards().then(function (cards) {
             $scope.flashCards = cards;
+            $scope.loading = false;
         });
     };
 
